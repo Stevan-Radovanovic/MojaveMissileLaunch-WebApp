ede56e574897ded9aa766e1e0cdb78b2358a8d65
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-23 00:38:47 +0200

    Added Command Panel component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CommandPanel from './CommandPanel/command-panel';
 
 function App() {
   return (
     <div className="App">
       <h1>Mojave Missile Launch</h1>
+      <CommandPanel />
+      <CommandPanel />
+      <CommandPanel />
+      <CommandPanel />
     </div>
   );
 }
---
+++ src/CommandPanel/command-panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const commandPanel = () => {
+  return <p>This is a Command Pannel</p>;
+};
+
+export default commandPanel;
