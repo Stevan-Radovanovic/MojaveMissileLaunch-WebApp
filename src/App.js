bc6826ff33a2ba088b427cd6167a57e4c47506ae
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-23 13:27:43 +0200

    Added Event Handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import CommandPanel from './CommandPanel/command-panel';
+import CommandPanel from './CommandPanel/Command-panel';
 
 function App() {
+  const mainframeHandler = () => {
+    console.log('Mainframe Activated');
+  };
+
   return (
     <div className="App">
       <h1>Mojave Missile Launch</h1>
       <CommandPanel version="1.0" />
-      <CommandPanel version="1.1">This is the Mainframe</CommandPanel>
+      <CommandPanel version="1.1">Mainframe Deactivated</CommandPanel>
       <CommandPanel version="1.2" />
       <CommandPanel version="1.3" />
+      <button onClick={mainframeHandler}>Activate Mainframe</button>
     </div>
   );
 }
